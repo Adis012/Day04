9e086d68060785c0bfb5c5d0dc310fbd89747acb
Adis012 <adis012@hotmail.com>
2021-03-11 12:13:38 +0100

    adding index

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,23 @@
+myRes = 18 / 9; /2/
+
+function main(18, 9){
+
+    var quotient;
+    var numOne
+    var numTwo
+    if(9 === 0){
+        quotient = "Divide by 0! Cannot divide by zero!";
+    } else {
+        /dividing one number by another/
+
+        /numOne, numTwo/
+
+    }
+
+    return quotient;
+
+}
+
+console.log(main(18, 9)); /2/
+module.exports = main;
 
