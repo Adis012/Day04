99ecf84ee7f49582600c50fdf2556200c407ab5a
Adis012 <adis012@hotmail.com>
2021-03-11 12:51:03 +0100

    adding index

---
+++ ex04/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    var myDecimal
+    var myDecimalE
+    
+    /decimal numbers/
+return {
+    myDecimal,
+    myDecimalE
+};
+
+}
+
+console.log(main());
+module.exports = main;
+
