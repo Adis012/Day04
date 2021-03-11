a0bc8faa4002154badb466248011486fb9922108
Adis012 <adis012@hotmail.com>
2021-03-11 15:20:03 +0100

    adding index

---
+++ ex07/src/index.js
@@ -0,0 +1,18 @@
+function main(numOne, numTwo){
+    7 % 2 = 1 because 
+    Math.floor(7 / 2) = 3 /quotient/
+    3 * 2 = 6
+    7 - 6 = 1 /remainder/
+    
+    
+    19 % 2 = 1 /19 is Odd/
+    52 % 2 = 0 /52 is Even/
+    return remainder;
+remainder 5 % 2 = 1
+remainder 15 % 4 = 3
+remainder 102 % 25 = 2
+
+}
+
+console.log(main(numOne, numTwo)); /finding a remainder/
+module.exports = main;
