01472645837eb2c1d9ecd5db88dfd039b7699d22
Adis012 <adis012@hotmail.com>
2021-03-11 12:38:53 +0100

    adding index

---
+++ ex03/src/index.js
@@ -0,0 +1,22 @@
+let x = 3;
+const y = x--;
+
+console.log(`x:${x}, y:${y}`);
+// expected output: "x:2, y:3"
+
+let a = 3;
+const b = --a;
+
+console.log(`a:${a}, b:${b}`);
+// expected output: "a:2, b:2"
+
+function main(){
+    var myRes = 16;
+    /i = 16 - 1/
+
+    return myRes;
+
+}
+
+console.log(main());
+module.exports = main;
