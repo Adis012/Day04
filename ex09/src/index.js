185b60b49bcc687071c17a86b8b9afeba3608430
Adis012 <adis012@hotmail.com>
2021-03-11 15:46:57 +0100

    adding index

---
+++ ex09/src/index.js
@@ -0,0 +1,17 @@
+function main(){
+    myVariable = myVariable * 8;
+    myVariable *= 8;
+
+    myVariable = myVariable / 8;
+    myVariable /= 8;
+    return {
+        firstNum = 6;
+        secondNum = 25;
+        thirdNum = 5.2;
+
+    };
+
+}
+
+console.log(main());
+module.exports main;
