5726fdb8ad0a40c5856d8e06789535303f2d96c2
Adis012 <adis012@hotmail.com>
2021-03-11 13:23:46 +0100

    adding index

---
+++ ex05/src/index.js
@@ -0,0 +1,12 @@
+function main(2.5, 2.0){
+    /2.5 * 2.0 = 5.0/
+    /5.5 * 5*5 = 30.25/
+    /100.25 * 43.20 = 4330.8/
+    return product;
+
+}
+
+console.log(main(2.5, 2.0)); /5/
+console.log(main(5.5 * 5.5); /30.25/
+console.log(main(100.25 * 43.20)); /4330.8/
+module.exports =main; 
