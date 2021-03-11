fda507d460bdbb0c10f19870af2bb7d9874f1e80
Adis012 <adis012@hotmail.com>
2021-03-11 15:00:23 +0100

    adding index

---
+++ ex06/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo){
+    /divide one decimal by another with JS/
+
+    /pocetak programiranja/
+var numOne; 5.0 / 2.0 = 2.5
+var numTwo; 16.5 / 5.5 = 3.0
+var myQuotient; 102.0 / 25-5 = 4.0
+return myQuotient;
+
+}
+
+console.log(main(numOne, numTwo)); /divide one decimal by another with JS/
+module.exports = main;
