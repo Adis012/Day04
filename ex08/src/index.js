9f179795a5c6bbc1ab234260cb4d8c434f7782e1
Adis012 <adis012@hotmail.com>
2021-03-11 15:35:26 +0100

    adding index

---
+++ ex08/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+    /coumpound assignment/
+    var myx = 12,
+    var myY = 19,
+    var myZ = 24
+    /augmendet addition and subtraction/
+    return {
+        myx,
+        myY,
+        myZ
+    };
+    
+}
+
+console.log(main());
+module.exports = main;
